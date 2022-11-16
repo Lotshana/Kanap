5fc382b6a800795cb2e88e3b5b5632fdc0070c26
Lotshana <nahh.design@gmail.com>
2022-11-16 12:59:27 +0100

    Correction des éléments du produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,8 +1,21 @@
 const idKanap = new URLSearchParams.get("id")
 
+const imgKanap = document.querySelector("item__img");
+const nameKanap = document.getElementById("title");
+const priceKanap = document.getElementById("price");
+const txtKanap = document.getElementById("description");
+
+/*function displayKanap(imgProduct){
+    imgKanap.innerHTML += 
+    `<img src="${imgProduct.imageUrl}" alt="${imgProduct.altTxt}">`;
+} */
+
 fetch("http://localhost:3000/api/products + idKanap")
     .then((response) => response.json())
-
+    .then(function displayKanap(imgProduct){
+        imgKanap.innerHTML += 
+        `<img src="${imgProduct.imageUrl}" alt="${imgProduct.altTxt}">`;
+    })
     .catch(function(error){
       alert("Toutes nos excuses, le canapé n'est pas disponible")
     });
