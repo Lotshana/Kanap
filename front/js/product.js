ba9316acac507afe812416d9e5706dbc1243fcde
Lotshana <nahh.design@gmail.com>
2022-11-23 18:46:55 +0100

    Modification du code javascript

--- front/js/product.js
+++ front/js/product.js
@@ -1,17 +1,25 @@
-const productId = new URLSearchParams.get("id")
+const url = new URL(document.location.href);
+const idProduct = url.searchParams.get("id");
 
-const img = document.querySelector("item__img");
 const title = document.getElementById("title");
+const img = document.getElementById("product__img");
 const price = document.getElementById("price");
 const description = document.getElementById("description");
-
-function displayProduct(kanap){
-    `title.innerHTML = kanap.name`;
-}
+const colors = document.getElementById("colors");
 
 
-fetch("http://localhost:3000/api/products + productId")
+fetch("http://localhost:3000/api/products + idProduct")
     .then(response => response.json())
+    .then(function(item) {
+      title.innerHTML = `${item.name}`
+      img.innerHTML = `<img src="${item.imageUrl}" alt="${item.altTxt}">`
+      price.innerHTML = `${item.price}`
+      description.innerHTML = `${item.description}`
+      for(let color of item.colors){
+        colors.innerHTML+=
+        `<option value="${color}">${color}</option>`
+      }
+    })
 
     .catch(function(error){
       alert("Toutes nos excuses, le canapé n'est pas disponible")
