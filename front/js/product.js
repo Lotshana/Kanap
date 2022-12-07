70192d6fd455bd80fcbb60dcf5eb2c06057da6fc
Lotshana <nahh.design@gmail.com>
2022-12-07 16:13:17 +0100

    Update js product panier

--- front/js/product.js
+++ front/js/product.js
@@ -22,6 +22,46 @@ fetch("http://localhost:3000/api/products/" + idProduct)
       }
     })
 
-    .catch(function(error){
+    .catch(function(error) {
       alert("Toutes nos excuses, le canapé n'est pas disponible")
     });
+
+
+    
+
+    const addLocalStorage = function(panier){
+      localStorage.setItem('panier', JSON.stringify(panier));
+    }
+
+    const ajoutPanier = () => {
+      let quantity = parseInt(document.getElementById('quantity').value);
+      let color = colorsOption.options[colorsOption.selectedIndex].value;    
+
+      let newBuy = {
+        "_id" : idProduct,
+        "quantity" : quantity,
+        "color" : color,
+      }
+      
+      let panier = localStorage.getItem('panier') ? JSON.parse(localStorage.getItem('panier')) : [];
+
+      let productExistIndex = false;
+      for (let i = 0; i < panier.length; i++) {
+        let productPanel = panier[i];
+
+        if (productPanel._id === productExistIndex._id) {
+          productExistIndex = i;
+        }
+      }
+
+      if (false !== productExistIndex) {
+        panier[productExistIndex].quantity = parseInt(panier[productExistIndex].quantity) + productExistIndex.quantity;
+      } else {
+        panier.push(produit);
+      }
+
+      addLocalStorage(panier);
+      window.location.replace("/front/html/cart.html");
+    }
+
+    document.getElementById('addToCart').addEventListener('click', function () {ajoutPanier(Kanap)})
