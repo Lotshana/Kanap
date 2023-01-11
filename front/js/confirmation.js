8f5e47a33c12e9016e9a7b9d6df809d4cde48004
Lotshana <nahh.design@gmail.com>
2023-01-11 17:50:27 +0100

    Commande confirmation

---
+++ front/js/confirmation.js
@@ -0,0 +1,11 @@
+// let orderId = document.getElementById("orderId");
+// let orderId = parseInt(document.getElementById('orderId').value);
+
+/* function(orderConfirmation) {
+    getOrderId.inner.HTML = `<span id="orderId"><!-- 65431343444684674 --></span>`
+}*/
+
+
+const orderConfirmation = () => {
+    let orderId = parseInt(document.getElementById('orderId').value);
+}
